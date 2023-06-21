8ee019fa001c03d4ab1a89ea977dbfd9c6042b58
facunare <arechaga.facundoet36@gmail.com>
2023-06-21 19:11:10 -0300

    no funciona

--- djangoTato/ia/chatbot.py
+++ djangoTato/ia/chatbot.py
@@ -1,24 +1,47 @@
-import tensorflow as tf
-from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.text import tokenizer_from_json
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+import json
+from sklearn.preprocessing import LabelEncoder
+
+import numpy as np
+from sentence_transformers import SentenceTransformer
+from scipy.spatial.distance import cosine
+from sklearn.preprocessing import LabelEncoder
+
+from sklearn.preprocessing import LabelEncoder
 
 class tatoBot:
-    def __init__(self, model_path, tokenizer_path, max_seq_length):
+    def __init__(self, model_path, tokenizer_path, max_length):
+        # Carga el modelo y el tokenizer
         self.model = load_model(model_path)
-        with open(tokenizer_path) as f:
-            data = f.read()
-            self.tokenizer = tokenizer_from_json(data)
-        self.max_seq_length = max_seq_length
-
+        self.tokenizer = self.load_tokenizer(tokenizer_path)
+        self.max_length = max_length
+        # Inicializa el LabelEncoder
+        self.label_encoder = LabelEncoder()
+        # Ajusta el LabelEncoder con las etiquetas conocidas
+        etiquetas = ['¡Hola! me llamo Tatobot', '¡Hola! Estoy muy bien ¿y tu?', 'La escuela se llama Almirante Guillermo Brown mejor conocida como ET36', 'El colegio queda en el Polo educativo Saavedra en la calle Galvan 3700', 'El colegio se llama Almirante Guillermo Brown', 'El nombre abreviado del colegio es ET36', 'El colegio se encuentra en el barrio de Saavedra']
+        self.label_encoder.fit(etiquetas)  # Reemplaza con las etiquetas adecuadas
+    
+    def load_tokenizer(self, tokenizer_path):
+        # Carga el tokenizer desde el archivo JSON
+        with open(tokenizer_path, 'r') as file:
+            tokenizer_json = file.read()
+        tokenizer = tokenizer_from_json(tokenizer_json)
+        return tokenizer
+    
     def preprocess_text(self, text):
-        sequences = self.tokenizer.texts_to_sequences([text])
-        sequences = pad_sequences(sequences, maxlen=self.max_seq_length)
-        return sequences
+        # Preprocesa el texto utilizando el tokenizer
+        text = text.lower()
+        text = self.tokenizer.texts_to_sequences([text])
+        text = pad_sequences(text, maxlen=self.max_length)
+        return text
+    
+    def responder_oracion(self, oracion):
+        oracion_codificada = self.preprocess_text(oracion)
+        prediccion = self.model.predict(oracion_codificada)
+        prediccion_clase = np.argmax(prediccion, axis=2)
+        respuesta_codificada = self.label_encoder.classes_[prediccion_clase[0]]
+        respuesta = respuesta_codificada
+        return respuesta
 
-    def response(self, text):
-        preprocessed_text = self.preprocess_text(text)
-        prediction = self.model.predict(preprocessed_text)
-        predicted_class_index = tf.argmax(prediction, axis=1).numpy()[0]
-        response = self.tokenizer.index_word[predicted_class_index+1]
-        return response
--- djangoTato/tatoApp/views.py
+++ djangoTato/tatoApp/views.py
@@ -11,8 +11,15 @@ def ask(request):
     if request.method == 'POST':
         oracion = request.POST['message']
         print(oracion)
-        detector = tatoBot(model_path='ia/model.h5', tokenizer_path='ia/tokenizer.json', max_seq_length=13)
-        respuesta = detector.response(oracion)
+        detector = tatoBot(model_path='ia/model.h5', tokenizer_path='ia/tokenizer.json', max_length=13)
+
+        # Etiquetas de entrenamiento
+        etiquetas = ['¡Hola! me llamo Tatobot', '¡Hola! Estoy muy bien ¿y tu?', 'La escuela se llama Almirante Guillermo Brown mejor conocida como ET36', 'El colegio queda en el Polo educativo Saavedra en la calle Galvan 3700', 'El colegio se llama Almirante Guillermo Brown', 'El nombre abreviado del colegio es ET36', 'El colegio se encuentra en el barrio de Saavedra']
+        # Ajustar el LabelEncoder con los datos de entrenamiento
+        detector.label_encoder.fit(etiquetas)
+
+        respuesta = detector.responder_oracion(oracion)
+        print(respuesta)
         return render(request, 'contacto.html', {'respuesta': respuesta})
     else:
         return render(request, 'contacto.html')

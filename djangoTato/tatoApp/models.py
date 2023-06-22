19f6f6019c39bf91dac68d0ce1e9ea406ebef9e2
facunare <arechaga.facundoet36@gmail.com>
2023-06-21 22:01:50 -0300

    jijijja

--- djangoTato/tatoApp/models.py
+++ djangoTato/tatoApp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from datetime import datetime
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Messages(models.Model):
+    content = models.TextField(max_length=180)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
--- djangoTato/tatoApp/views.py
+++ djangoTato/tatoApp/views.py
@@ -1,25 +1,29 @@
 from django.shortcuts import render
 from ia.chatbot import tatoBot
+from .models import Messages
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
 
 def contacto(request):
-    return render(request, 'contacto.html')
+    mensajes = Messages.objects.all()
+    return render(request, 'contacto.html', {
+        'mensajes':mensajes
+    })
+   
 
 def ask(request):
     if request.method == 'POST':
-        oracion = request.POST['message']
-        print(oracion)
-        detector = tatoBot(model_path='ia/model.h5', tokenizer_path='ia/tokenizer.json', max_length=13)
-
-        # Etiquetas de entrenamiento
-        etiquetas = ['¡Hola! me llamo Tatobot', '¡Hola! Estoy muy bien ¿y tu?', 'La escuela se llama Almirante Guillermo Brown mejor conocida como ET36', 'El colegio queda en el Polo educativo Saavedra en la calle Galvan 3700', 'El colegio se llama Almirante Guillermo Brown', 'El nombre abreviado del colegio es ET36', 'El colegio se encuentra en el barrio de Saavedra']
-        # Ajustar el LabelEncoder con los datos de entrenamiento
-        detector.label_encoder.fit(etiquetas)
-
-        respuesta = detector.responder_oracion(oracion)
-        print(respuesta)
-        return render(request, 'contacto.html', {'respuesta': respuesta})
+        
+        mensaje = Messages.objects.create(content = request.POST['message'])
+        
+        # oracion = request.POST['message']
+        # detector = tatoBot(model_path='ia/model.h5', tokenizer_path='ia/tokenizer.json', max_length=13)
+        # etiquetas = ['¡Hola! me llamo Tatobot', '¡Hola! Estoy muy bien ¿y tu?', 'La escuela se llama Almirante Guillermo Brown mejor conocida como ET36', 'El colegio queda en el Polo educativo Saavedra en la calle Galvan 3700', 'El colegio se llama Almirante Guillermo Brown', 'El nombre abreviado del colegio es ET36', 'El colegio se encuentra en el barrio de Saavedra']
+        # detector.label_encoder.fit(etiquetas)
+        # respuesta = detector.responder_oracion(oracion)
+        # return render(request, 'contacto.html', {'respuesta': respuesta})
+        return render(request, 'contacto.html')
     else:
+    
         return render(request, 'contacto.html')

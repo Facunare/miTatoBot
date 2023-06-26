0cde2a614aca558e5fc270791e67591f9c6e1206
facunare <arechaga.facundoet36@gmail.com>
2023-06-26 12:46:34 -0300

    hola

--- djangoTato/tatoApp/static/chat.js
+++ djangoTato/tatoApp/static/chat.js
@@ -13,64 +13,72 @@ function getCookie(name) {
     }
     return cookieValue;
 }
-
 function sendMessage(event) {
-    event.preventDefault()
-    console.log("aca")
-    var formMessage = new FormData(document.getElementById('formMessage'));
-  
-    fetch('../ask/', {
-      method: 'POST',
-      body: formMessage,
-      headers: {
-        'X-CSRFToken': getCookie('csrftoken'),
-        'X-Requested-With': 'XMLHttpRequest'
-      }
-    })
-      .then(function (res) {
-        return res.json();
-      })
-      .then(function (data) {
-        var message = data.mensaje;
-        var respuesta = data.respuesta;
-        const input = document.querySelector(".input")
-        var messagesContainer = document.querySelector('.messages .msg');
-        if(input.value !== ""){
+  event.preventDefault();
+  console.log("aca");
+  var formMessage = new FormData(document.getElementById('formMessage'));
 
-          var messageElement = document.createElement('p');
-          messageElement.style.color = "black"
-          messageElement.textContent = message;
-          messageElement.classList.add('question');
-          
-          var timestampElement = document.createElement('span');
-          timestampElement.style.color = "black"
-          timestampElement.textContent = getCurrentTime();
-          messageElement.appendChild(timestampElement);
-          
-          messagesContainer.appendChild(messageElement);
-    
-          if (respuesta) {
-            var responseElement = document.createElement('p');
-            responseElement.textContent = respuesta;
-            responseElement.classList.add('response');
-            responseElement.style.color = "black"
-            var responseTimestampElement = document.createElement('span');
-            responseTimestampElement.textContent = getCurrentTime();
-            responseElement.appendChild(responseTimestampElement);
-    
-            messagesContainer.appendChild(responseElement);
-  
-          }
-          input.value = ""
-        }else{
-          console.log("hola")
+  fetch('../ask/', {
+    method: 'POST',
+    body: formMessage,
+    headers: {
+      'X-CSRFToken': getCookie('csrftoken'),
+      'X-Requested-With': 'XMLHttpRequest'
+    }
+  })
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (data) {
+      var message = data.mensaje;
+      var respuesta = data.respuesta;
+      const input = document.querySelector(".input");
+      var messagesContainer = document.querySelector('.messages .msg');
+      if (input.value !== "") {
+        var messageElement = document.createElement('p');
+        messageElement.style.color = "black";
+        messageElement.textContent = message;
+        messageElement.classList.add('question');
+
+        var timestampElement = document.createElement('span');
+        timestampElement.style.color = "black";
+        timestampElement.textContent = getCurrentTime();
+        messageElement.appendChild(timestampElement);
+
+        messagesContainer.appendChild(messageElement);
+
+        if (respuesta) {
+          var responseElement = document.createElement('p');
+          responseElement.textContent = respuesta;
+          responseElement.classList.add('response');
+          responseElement.style.color = "black";
+          var responseTimestampElement = document.createElement('span');
+          responseTimestampElement.textContent = getCurrentTime();
+          responseElement.appendChild(responseTimestampElement);
+
+          messagesContainer.appendChild(responseElement);
         }
 
-      })
-      .catch(function (error) {
-        console.error('Error:', error);
-      });
-  }
+        input.value = "";
+      }
+
+        var deleteForm = document.createElement('form');
+        deleteForm.action = 'deleteChat/';
+        deleteForm.method = 'post';
+        deleteForm.classList.add('delete');
+
+        var deleteButton = document.createElement('button');
+        deleteButton.innerHTML = '<i class="bx bx-trash"></i>';
+
+        deleteForm.appendChild(deleteButton);
+        messagesContainer.appendChild(deleteForm);
+      
+    })
+    .catch(function (error) {
+      console.error('Error:', error);
+    });
+}
+
   
   function getCurrentTime() {
     var now = new Date();
--- djangoTato/tatobot/settings.py
+++ djangoTato/tatobot/settings.py
@@ -118,7 +118,7 @@ LOGIN_REDIRECT_URL = '/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'tatoApp/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
